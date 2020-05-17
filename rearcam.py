55b921b038aaf5a4d32858f113f80abf8fc1442b
Blake Bailey <blake@Blakes-MacBook-Pro.local>
2020-05-17 14:00:58 -0500

    Going to try to stream from boot on pi now.

--- rearcam.py
+++ rearcam.py
@@ -2,6 +2,8 @@
 
 import subprocess
 from subprocess import call
+import glob
+import os
 
 SDcard_threshold = 95  # % of SD card above which we'll delete the oldest .h264 files
 
@@ -14,22 +16,34 @@ def space_used():    # function displays amt of space left on device
         if it_num == 1:
             storage = line_list
         it_num += 1
-    print "Card size: %s Used: %s  Available: %s  Percent used: %s  SD Threshold: %d" % (storage[1], storage[2], storage[3], storage[4], SDcard_threshold)
+    print "Card size: %s Used: %s  Available: %s  Percent used: %s  SD Threshold: %d%%" % (storage[1], storage[2], storage[3], storage[4], SDcard_threshold)
     percent_used = int(storage[4][0:-1])
     if percent_used > SDcard_threshold:
         print "SD card %s full. Not enough space left! Removing oldest .h264 file" % storage[4]
         remove_a_file()  # call our function to make some space on the card
         space_used()     # call this function recursively until enough space on card
 
-def practice():
-    cmd = "stat --printf='%Y %n\0' /Users/blake/Desktop/*.txt" | sort -z | sed -zn '1s/[^ ]\{1,\} //p' |  xargs -0 rm"
-    call('cmd', shell=True)
-# from subprocess import call
+def removeOldestFile():
 
+    try:
+        list_of_files = glob.glob('/Users/blake/Desktop/*.txt')
+        oldest_file = min(list_of_files, key=os.path.getctime)
+        os.remove(oldest_file)
+        pass
+    except ValueError:
+        pass
+
+# def recordVideo():
+
+def streamVideo():
+    call (["raspivid -t 0"], shell=True)
+
+
+def main():
+    streamVideo()
 
 ###############################################################################
 if __name__ == '__main__':
-    space_used()
-    practice()
+    main()
 
 ###############################################################################
