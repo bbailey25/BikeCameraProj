fcb83948b0447deadf7f4dfe94f6d42e6e9673c3
Blake Bailey <blake@Blakes-MacBook-Pro.local>
2020-05-17 14:27:00 -0500

    Recording video now as well.

--- rearcam.py
+++ rearcam.py
@@ -4,6 +4,7 @@ import subprocess
 from subprocess import call
 import glob
 import os
+import datetime
 
 SDcard_threshold = 95  # % of SD card above which we'll delete the oldest .h264 files
 
@@ -18,10 +19,10 @@ def space_used():    # function displays amt of space left on device
         it_num += 1
     print "Card size: %s Used: %s  Available: %s  Percent used: %s  SD Threshold: %d%%" % (storage[1], storage[2], storage[3], storage[4], SDcard_threshold)
     percent_used = int(storage[4][0:-1])
-    if percent_used > SDcard_threshold:
-        print "SD card %s full. Not enough space left! Removing oldest .h264 file" % storage[4]
-        remove_a_file()  # call our function to make some space on the card
-        space_used()     # call this function recursively until enough space on card
+    # if percent_used > SDcard_threshold:
+    #     print "SD card %s full. Not enough space left! Removing oldest .h264 file" % storage[4]
+    #     remove_a_file()  # call our function to make some space on the card
+    #     space_used()     # call this function recursively until enough space on card
 
 def removeOldestFile():
 
@@ -35,12 +36,16 @@ def removeOldestFile():
 
 # def recordVideo():
 
-def streamVideo():
-    call (["raspivid -t 0"], shell=True)
-
+def streamRecordVideo():
+    while True:
+        dt = datetime.datetime.now()
+        print dt
+        fileName = dt.hour + "_" + dt.second
+        command = "raspivid -t 300000 -vs -o /home/pi/Videos/" + fileName + ".h264"
+        call (command, shell=True)
 
 def main():
-    streamVideo()
+    streamRecordVideo()
 
 ###############################################################################
 if __name__ == '__main__':
